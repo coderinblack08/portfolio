85c7bbf8c46e096a91cbf567f25f916fde046dc3
coderinblack <kevinlu.email@gmail.com>
2020-10-15 15:22:22 -0700

    :wrench: Change for chromebooks

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       className={`${styles.header} sm:h-screen relative`}
       style={{
         maxHeight: '920px',
-        minHeight: '800px',
+        minHeight: '780px',
       }}
     >
       <Navbar />
